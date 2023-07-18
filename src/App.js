880faefd9030bd2da70a795ca4a3e708b3a871ea
JumanazarB <jumanazarboqiyev5@gmail.com>
2023-07-19 00:01:52 +0500

    added 14 th lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [name,setName] = useState('Assalomu alaykum')
-  const [events] = useState([
-    {title:"Barselona", rank:1},
-    {title:"ManCity", rank:2},
-    {title:"Bavariya", rank:3}]
+  const [name, setName] = useState('Assalomu alaykum')
+  const [shownBoolen, setShownBoolen] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "Barselona", rank: 1 },
+    { title: "ManCity", rank: 2 },
+    { title: "Bavariya", rank: 3 }]
+
   )
-  function nameEdit(){
+  function nameEdit() {
     return setName("Xush kelibsiz, Boqiyev Jumanazar")
   }
+
+  function itemDelete(id) {
+    setEvents((prev) => {
+      return prev.filter((item) => {
+        return item.rank !== id
+      })
+    })
+  }
+
+
   return (
     <div className="App">
       <h1>{name}</h1>
-      <button onClick = {nameEdit}>Welcome</button>
-      {events.map((event,id)=>{
-        return (
-        <div key = {event.id}>
-          <h2>{event.title}</h2>
-        </div>)
-      })}
+      <button onClick={nameEdit}>Welcome</button>
+      <hr></hr>
+      {shownBoolen && <button onClick={()=> setShownBoolen(false)}>hide</button>}
+      {!shownBoolen && <button onClick={()=> setShownBoolen(true)}>show</button>}
+      {shownBoolen && <div>{events.length === 0 && <h3>Malumotlar yuq</h3>}
+      {
+        events.map((event) => {
+          return (
+            <div key={event.rank}>
+              <h2>{event.title}</h2>
+              <button onClick={() => itemDelete(event.rank)}>Delete</button>
+            </div>)
+        })
+      }</div>
+      }
     </div>
   );
 }
