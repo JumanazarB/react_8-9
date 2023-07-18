cf4ef6530abdd38651d1eb15fd413103fb15809a
JumanazarB <jumanazarboqiyev5@gmail.com>
2023-07-18 22:39:49 +0500

    added lesson 12

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [name,setName] = useState('Jumanazar')
-
+  const [name,setName] = useState('Assalomu alaykum')
+  const [events] = useState([
+    {title:"Barselona", rank:1},
+    {title:"ManCity", rank:2},
+    {title:"Bavariya", rank:3}]
+  )
   function nameEdit(){
-    return setName("Boqiyev Jumanazar")
+    return setName("Xush kelibsiz, Boqiyev Jumanazar")
   }
   return (
     <div className="App">
-      <h1 onClick = {nameEdit}>salom {name}</h1>
+      <h1>{name}</h1>
+      <button onClick = {nameEdit}>Welcome</button>
+      {events.map((event,id)=>{
+        return (
+        <div key = {event.id}>
+          <h2>{event.title}</h2>
+        </div>)
+      })}
     </div>
   );
 }
